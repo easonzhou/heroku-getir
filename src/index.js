019c0ece04498071f08e867e4fda4bd40237379e
Yengas <yigitcan@hotmail.com.tr>
2017-03-01 10:29:55 +0300

    Add sample routes and body parser.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const config = require('./config'),
+      bodyParser = require('body-parser'),
+      express = require('express'),
+      app = express();
+
+app.use(bodyParser.json({ type: () => true }));
+
+app.get('/', (req, res) => res.json({ hello: 'world' }));
+app.get('/getRecord', (req, res) => res.json({ message: "Get method not allowed! Please use POST." }));
+app.post('/getRecord', (req, res) => res.json({ message: `We're print you back the request, you have sent us.`, body: req.body }));
+
+app.listen(config.port, () => console.log(`Started listening on port ${config.port}.`));
